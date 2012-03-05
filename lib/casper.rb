0e8a29656cfe041e2e500860e8f9116fc7414706
Paolo Perego <thesp0nge@gmail.com>
2012-03-05 13:05:00 +0100

    Added some callbacks for SIGINFO e SIGUSR

--- lib/casper.rb
+++ lib/casper.rb
@@ -4,23 +4,48 @@ require "casper/version"
 
 module Casper
   class Proxy < WEBrick::HTTPProxyServer
+    attr_reader :req_count
+    attr_reader :hosts
 
     def initialize(config={})
-      access_log = [
-        [$stderr, WEBrick::AccessLog::COMMON_LOG_FORMAT],
-        [$stderr, WEBrick::AccessLog::REFERER_LOG_FORMAT],
-      ]
+      @req_count = 0
+      @hosts=[]
       config[:Port] ||= 8080
-      config[:AccessLog] = access_log
-      config[:ProxyContentHandler] = Proc.new do |req, res| Casper::Proxy.log_requests(req, res) end
+      config[:AccessLog] = []
+      config[:ProxyContentHandler] = Proc.new do |req, res| 
+        log_requests(req, res) 
+      end
 
       super(config)
     end
 
-    private 
-    def self.log_requests(req, res)
-      $stdout.puts "#{req.request_line.chomp}"
+    def info
+      $stdout.puts "[#{Time.now}] INFO #{@req_count} requests to #{@hosts.count} unique hosts"
+    end
 
+    def dump
+      $stdout.puts "Hosts we communicate with "
+      if (@hosts.count == 0)
+        $stdout.puts "None\n"
+
+      else
+        @hosts.each do |h|
+          $stdout.puts " >>> #{h}\n"
+        end
+      end
+    end
+
+    private 
+    def log_requests(req, res)
+      $stdout.puts "[#{Time.now}] #{req.request_line.chomp}\n"
+      if @hosts.index(req.host).nil?
+        @hosts << req.host
+      end
+      inc_req_count
+    end
+
+    def inc_req_count
+      @req_count += 1
     end
   end
 end
